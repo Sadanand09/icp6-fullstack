ff7debc158a12ac6dbdb394603088150acf203cb
omnandurkar <nandurkarom172@gmail.com>
2024-04-07 15:46:30 +0530

    Adde the Firebase Login

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -52,16 +52,16 @@ export default function Footer() {
 
                 </footer>
 
-                <div class=" offset-md-1  mx-auto">
+                <div className=" offset-md-1  mx-auto">
                     <form className='w-75 gap-2 flex-sm-row mx-auto gap-2'>
                         <div className='d-flex flex-column flex-sm-row  gap-2'>
                             <h5>Subscribe to our newsletter</h5>
                             <p>Monthly digest of what's new and exciting from us.</p>
                         </div>
-                        <div class="d-flex flex-column flex-sm-row  gap-2">
-                            <label for="newsletter1" class="visually-hidden">Email address</label>
-                            <input id="newsletter1" type="text" class="form-control" placeholder="Email address" />
-                            <button class="btn btn-info" type="button" fdprocessedid="6bvxu">Subscribe</button>
+                        <div className="d-flex flex-column flex-sm-row  gap-2">
+                            <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
+                            <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
+                            <button className="btn btn-info" type="button" fdprocessedid="6bvxu">Subscribe</button>
                         </div>
                     </form>
                 </div>
