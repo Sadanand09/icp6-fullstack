a3f3e54c6bf4a32ed198729217b045127a117f42
kiran-jaswant <kiranjaswant08@gmail.com>
2024-04-07 15:54:20 +0530

    destination page modified

--- client/src/views/Destination/Destination.js
+++ client/src/views/Destination/Destination.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import bookingData from '../../configs/Booking/Transport.json';
+import './destination.css'
 
 
 export default function Destination() {
@@ -11,18 +12,30 @@ export default function Destination() {
         return <div>Booking not found</div>;
     }
 
+
     return (
-        <div className="container col-xxl-12 px-4 py-5 d-flex mx-auto">
-            <div className="mx-auto d-flex">
-                <div className="col-10 col-sm-8 col-lg-8 border rounded-2 p-3">
-                    <h2>{booking.title}</h2>
+        <div>
+            <div className="container-fluid p-0 position-relative ">
+                <img src={booking.poster} alt="Poster " className="img-fluid w-100 " style={{ height: '570px', filter: 'brightness(70%)' }} />
+                <div className="position-absolute top-0 start-50 mt-5 translate-middle-x text-center text-white">
+                    <h1 className="mb-4">{booking.tag}</h1>
                     <p>{booking.description}</p>
-                    <p>Booking ID: {booking.id}</p>
                 </div>
-                <div>
-                        <img src={booking.img} alt="Transport" className="img-fluid rounded-2 border"/>
-                    </div>
             </div>
+
+            <div className="container col-xxl-12 px-4 py-5  mx-auto">
+                <div className="mx-auto d-flex">
+                    <div className="col-10 col-sm-8 col-lg-8 border rounded-2 p-3">
+                        <h2>{booking.title}</h2>
+                        <p>{booking.description}</p>
+                        <p>Booking ID: {booking.id}</p>
+                    </div>
+                    <div>
+                        <img src={booking.img} alt="Transport" className="img-fluid rounded-2 border" />
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
